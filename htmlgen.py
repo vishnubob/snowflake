8a98cb0ea091bd1503c8ae9048fd2385d6e7eb57
Giles Hall <vishnubob@github>
2012-12-02 00:48:29 -0500

    nice html table generator

---
+++ htmlgen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env pypy
+import cPickle as pickle
+import os
+from snowflake import *
+
+def add_example(name):
+    print "Processing %s..." % name
+    ret = ''
+    pfn = "%s.pickle" % name
+    f = open(pfn)
+    sf = pickle.load(f)
+    env = sf.environment
+    env_keys = env.keys()
+    env_keys.sort()
+    caption = ["%s = %s" % (key, env[key]) for key in env_keys]
+    caption = str.join(', ', caption)
+    ret += "<div style='border: 2px'>\n"
+    ret += "<h1>%s</h1>\n" % name
+    ret += "<img src='%s.bmp' />\n" % name
+    ret += "<p />%s\n" % caption
+    ret += "</div>\n"
+    return ret
+
+HTML = """<html><head><title>Mah Sn0wFl4K3z!@!##@</title></head>
+<body>%s</body>
+</html>"""
+
+if __name__ == "__main__":
+    body = ''
+    for fn in os.listdir("."):
+        if fn.endswith(".pickle"):
+            body += add_example(fn.split(".")[0])
+    html = HTML % body
+    fn = "snowflakes.html"
+    f = open(fn, 'w')
+    f.write(html)
